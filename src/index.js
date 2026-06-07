64c533ef3537364697955cd0fa3d0055a0947a8c
Muni Chaurasiya <199281592+Muni923@users.noreply.github.com>
2026-06-07 17:24:40 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-const UserRepository=require('././repository/user-repository')
+const UserService = require('./services/user-service');
+// const UserRepository=require('././repository/user-repository')
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
 
@@ -19,14 +20,24 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
     console.log(`Server Started on Port: ${PORT}`);
-        const repo = new UserRepository();
-        const response = await repo.getById(2);
-        console.log(response); 
+
+        // const repo = new UserRepository();
+        // const response = await repo.getById(2);
+        // console.log(response); 
+
 
         // const incomingpassword = '123456';
         // const user = await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
         // console.log(response);
+
+        const service = new UserService();
+        const newToken = service.createToken({email: 'abcd@gmail.com', id: 1});
+        console.log("new token is", newToken);
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY2RAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTc4MDgzMzE4NywiZXhwIjoxNzgwOTE5NTg3fQ.UhkUqWTM3a0u0wnNhrkkUiNB4ZkeqNHRwtq03PVvjho';
+        const response = service.verifyToken(token);
+        console.log(response);
+
 });
 }   
 prepareAndStartServer();
